fed836522d784ee24ebf10a4b6d9241cf31309a6
Lelton Borges <leltondev@gmail.com>
2021-10-11 21:36:38 -0300

    Transacao alterantive

--- src/main/java/br/com/livraria/transacao/MeuGerenciadorTransacao.java
+++ src/main/java/br/com/livraria/transacao/MeuGerenciadorTransacao.java
@@ -1,13 +1,28 @@
 package br.com.livraria.transacao;
 
+import br.com.cdi.api.lib.transaction.Transacionado;
 import br.com.cdi.api.lib.transaction.TransactionDefault;
 
+import javax.annotation.Priority;
+import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Intercepted;
 import javax.enterprise.inject.Specializes;
+import javax.enterprise.inject.Typed;
+import javax.inject.Inject;
+import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import javax.persistence.EntityManager;
 
-@Specializes
-public class MeuGerenciadorTransacao extends TransactionDefault {
+//@Specializes // Usado com herança, uma especialização da TransactionDefault
+@Alternative // Sobrepoe um bean já existente, ou add no bean.xml ou @Priority
+// no bean.xml não fica sendo utilizado em outros bean de outro projeto ou lib
+@Priority(Interceptor.Priority.APPLICATION) // add de modo global para todos os outros beans
+@Typed(Transacionado.class) // atender apenas a transacionado
+public class MeuGerenciadorTransacao implements Transacionado {
     private static final long serialVersionUID = -793331489933550997L;
+
+    @Inject
+    private EntityManager manager;
 
     @Override
     public Object executeTransacao(InvocationContext context) {
